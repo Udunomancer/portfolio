60e5c893d97091197efd2333fd00559ae4bcd9ea
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 18:01:15 -0500

    added basic Header and Footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import {HashRouter as Router, Route} from "react-router-dom";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import Portfolio from "./Pages/Portfolio/Portfolio";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Header />
     <Router>
       <Route exact path="/" component={About} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/portfolio" component={Portfolio} />
     </Router>
+    <Footer />
+    </>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />

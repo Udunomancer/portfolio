6be68c6a0af3c536164c4c65d249621256180041
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 17:57:34 -0500

    added routes to all main pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import {HashRouter as Router, Route} from "react-router-dom";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Portfolio from "./Pages/Portfolio/Portfolio";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/portfolio" component={Portfolio} />
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Portfolio() {
+    return (
+        <h1>Hello World - Portfolio</h1>
+    )
+}
+
+export default Portfolio;

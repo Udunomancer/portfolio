242096740d120cf51f947b701326a11d1d79397e
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 19:58:07 -0500

    basic Project framework

---
+++ src/Components/Project/Project.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Project() {
+    return (
+        <section className="col-lg-6">
+            <a href="#" target="_blank">
+                <img src="https://via.placeholder.com/300x300" alt="Alt Text" className="img-fluid mb-3" />
+            </a>
+        </section>
+    )
+}
+
+export default Project;

9db12ecb32ada04ea0f4d55af72648e642f21a50
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 21:05:41 -0500

    additional Contact page content editing

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import resume from "../../Assets/Resume.pdf";
 
 function Contact() {
     return (
@@ -11,12 +12,34 @@ function Contact() {
                         </div>
                     </div>
                     <div className="row m-2 py-2">
-                        <div className="col-sm-10">
-                            <ul>
-                                <li><a href="#">LinkedIn</a></li>
-                                <li><a href="#">LinkedIn</a></li>
-                                <li><a href="#">LinkedIn</a></li>
-                            </ul>
+                        <div className="col-sm-4 border-right">
+                            <p><strong>GitHub: </strong><br></br><a href="https://github.com/Udunomancer" target="_blank">github.com/Udunomancer</a></p>
+                            <p><strong>LinkedIn: </strong><br></br><a href="https://www.linkedin.com/in/kyle-chriscoe/" target="_blank">linkedin.com/in/kyle-chriscoe</a></p>
+                            <p><strong>Resume: </strong><br></br><a href={resume} target="_blank">Click Here</a></p>
+                            <p><strong>Email: </strong><br></br><a href="mailto:kyle717@gmail.com">kyle717@gmail.com</a></p>
+                            <p><strong>Phone: </strong><br></br>(404) 725-3435</p>
+                        </div>
+                        <div className="col-sm-8">
+                            <form>
+                                <div className="form-group row">
+                                    <label for="email" className="col-sm-2 col-form-label">Email</label>
+                                    <div className="col-sm-10">
+                                        <input type="email" className="form-control" id="email" placeholder="Your Email Address"></input>
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="message" className="col-sm-2 col-form-label">Message</label>
+                                    <div className="col-sm-10">
+                                        <textarea className="form-control" id="message" rows="4" placeholder="Your Message"></textarea>
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <div className="col-sm-2"></div>
+                                    <div className="col-sm-10">
+                                        <button type="submit" class="btn btn-primary">Message Me!</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>

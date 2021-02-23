40671a6b18e07f5802934fe88e2e83a249ec0dd4
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 21:45:29 -0500

    corrected error in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import "./Footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <div class="container">
+            <div className="container">
                 <div className="row justify-content-md-center">
-                    <div class="col-md-auto">
-                        <span class="text-muted">&#169; Copyright 2021 Kyle Chriscoe</span>
+                    <div className="col-md-auto">
+                        <span className="text-muted">&#169; Copyright 2021 Kyle Chriscoe</span>
                     </div>
                 </div>
             </div>

ffc04d665bc11d619d6c8edff70cc820af7622c0
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 22:19:57 -0500

    added image to About page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import resume from "../../Assets/Resume.pdf";
 
 function Contact() {
+    
+    // const [message, setMessage] = useState({
+    //     subject: "",
+    //     body: "",
+    //     href: ""
+    // })
+
+    // function handleInputChange(event) {
+    //     const name = event.target.name;
+    //     const value = event.target.value;
+    //     setMessage({...message, [name]: value});
+    //     setMessage({...message, href: `mailto:kyle717@gmail.com?subject=${message.subject}&body=${message.body}`})
+    // }
+    
     return (
         <div className="container">
             <div className="row">
@@ -12,25 +26,28 @@ function Contact() {
                         </div>
                     </div>
                     <div className="row m-2 py-2">
-                        <div className="col-sm-4 border-right">
+                        <div className="col-sm-6 border-right">
                             <p><strong>GitHub: </strong><br></br><a href="https://github.com/Udunomancer" target="_blank">github.com/Udunomancer</a></p>
                             <p><strong>LinkedIn: </strong><br></br><a href="https://www.linkedin.com/in/kyle-chriscoe/" target="_blank">linkedin.com/in/kyle-chriscoe</a></p>
                             <p><strong>Resume: </strong><br></br><a href={resume} target="_blank">Click Here</a></p>
+                        </div>
+                        <div className="col-sm-6">
                             <p><strong>Email: </strong><br></br><a href="mailto:kyle717@gmail.com">kyle717@gmail.com</a></p>
                             <p><strong>Phone: </strong><br></br>(404) 725-3435</p>
                         </div>
-                        <div className="col-sm-8">
+                        {/* <div className="col-sm-8">
                             <form>
+                                <h5>Email me!</h5>
                                 <div className="form-group row">
-                                    <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                                    <label htmlFor="subject" className="col-sm-2 col-form-label">Subject</label>
                                     <div className="col-sm-10">
-                                        <input type="email" className="form-control" id="email" placeholder="Your Email Address"></input>
+                                        <input type="text" className="form-control" id="subject" name="subject" value={message.subject} onChange={handleInputChange} placeholder="Your Message Subject"></input>
                                     </div>
                                 </div>
                                 <div className="form-group row">
                                     <label htmlFor="message" className="col-sm-2 col-form-label">Message</label>
                                     <div className="col-sm-10">
-                                        <textarea className="form-control" id="message" rows="4" placeholder="Your Message"></textarea>
+                                        <textarea className="form-control" id="message" name="body" rows="4" placeholder="Your Message Body" value={message.body} onChange={handleInputChange}></textarea>
                                     </div>
                                 </div>
                                 <div className="form-group row">
@@ -40,7 +57,7 @@ function Contact() {
                                     </div>
                                 </div>
                             </form>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
 

7cd5518bea3a3d3c65d8047ebae9559b611daa4c
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 20:10:16 -0500

    default formatting for Project components

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function Project() {
     return (
-        <section className="col-lg-6">
-            <a href="#" target="_blank">
-                <img src="https://via.placeholder.com/300x300" alt="Alt Text" className="img-fluid mb-3" />
-            </a>
+        <section className="col-lg-4">
+            <div className="card">
+                <img src="https://via.placeholder.com/300x300" alt="Alt Text" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title">Project Title</h5>
+                    <span>
+                        <a href="#" className="btn btn-primary">Repository</a>
+                        <a href="#" className="btn btn-primary">Page</a>
+                    </span>
+                </div>
+            </div>
         </section>
     )
 }
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -3,16 +3,23 @@ import Project from "../../Components/Project/Project";
 
 function Portfolio() {
     return (
-        <div class="container">
-            <div class="row">
-                <div className="col-md-9 bg-white py-3 mx-2">
-                    <div class="row mx-2">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 bg-white py-3 mx-2">
+                    <div className="row mx-2">
                         <div className="col-sm-12">
                             <h1 className="border-bottom py-3"><strong>Portfolio</strong></h1>
                         </div>
                     </div>
                     <div className="row m-2 py-2">
-                        
+                        <Project />
+                        <Project />
+                        <Project />
+                    </div>
+                    <div className="row m-2 py-2">
+                        <Project />
+                        <Project />
+                        <Project />
                     </div>
                 </div>
             </div>

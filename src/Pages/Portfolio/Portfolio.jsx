1c0b1f0f0ad8981433d21a9404143aed5053e21e
Kyle Chriscoe <kyle717@gmail.com>
2021-02-22 19:54:13 -0500

    added framework for portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Project from "../../Components/Project/Project";
 
 function Portfolio() {
     return (
-        <h1>Hello World - Portfolio</h1>
+        <div class="container">
+            <div class="row">
+                <div className="col-md-9 bg-white py-3 mx-2">
+                    <div class="row mx-2">
+                        <div className="col-sm-12">
+                            <h1 className="border-bottom py-3"><strong>Portfolio</strong></h1>
+                        </div>
+                    </div>
+                    <div className="row m-2 py-2">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 

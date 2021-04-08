2c0f5fa94f0ce2a07970303cc70f32192affa10c
Kyle Chriscoe <kyle717@gmail.com>
2021-04-08 17:12:28 -0400

    added additional Projects to the Project file

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -5,38 +5,37 @@ const Projects = [
         description: "Text about this project",
         repoLink: "https://github.com/Udunomancer/emp-directory",
         pageLink: "https://udunomancer.github.io/emp-directory",
+    },{
+        imgSrc: require("../Assets/Images/budget-tracker.png").default,
+        title: "Budget Tracker",
+        description: "Text about this project",
+        repoLink: "https://github.com/Udunomancer/gt-ft-budget-tracker",
+        pageLink: "https://budget-tracker-1234.herokuapp.com/",
+    },{
+        imgSrc: require("../Assets/Images/workout-tracker.png").default,
+        title: "Fitness Tracker",
+        description: "Text about this project",
+        repoLink: "https://github.com/Udunomancer/gt-ft-workout-tracker",
+        pageLink: "https://kyle-fitness.herokuapp.com",
+    },{
+        imgSrc: require("../Assets/Images/game-board.png").default,
+        title: "The Game Board",
+        description: "Text about this project",
+        repoLink: "https://github.com/Udunomancer/project2-krypton",
+        pageLink: "https://thegameboard.herokuapp.com",
+    },{
+        imgSrc: require("../Assets/Images/burger-buddy.png").default,
+        title: "Burger Buddy",
+        description: "Text about this project",
+        repoLink: "https://github.com/Udunomancer/gt-ft-burger-buddy",
+        pageLink: "https://serene-lowlands-21111.herokuapp.com/",
+    },{
+        imgSrc: require("../Assets/Images/note-taker.png").default,
+        title: "Note Taker",
+        description: "Text about this project",
+        repoLink: "https://github.com/Udunomancer/gt-ft-note-taker",
+        pageLink: "https://guarded-island-65290.herokuapp.com/",
     }
-    // ,{
-    //     imgSrc: ,
-    //     title: ,
-    //     description: ,
-    //     repoLink: ,
-    //     pageLink: ,
-    // },{
-    //     imgSrc: ,
-    //     title: ,
-    //     description: ,
-    //     repoLink: ,
-    //     pageLink: ,
-    // },{
-    //     imgSrc: ,
-    //     title: ,
-    //     description: ,
-    //     repoLink: ,
-    //     pageLink: ,
-    // },{
-    //     imgSrc: ,
-    //     title: ,
-    //     description: ,
-    //     repoLink: ,
-    //     pageLink: ,
-    // },{
-    //     imgSrc: ,
-    //     title: ,
-    //     description: ,
-    //     repoLink: ,
-    //     pageLink: ,
-    // }
 ];
 
 export default Projects;

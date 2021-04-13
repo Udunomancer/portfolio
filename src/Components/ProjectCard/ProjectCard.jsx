b4f39fe8b55e10428958e67811dac2479fce03ba
Kyle Chriscoe <kyle717@gmail.com>
2021-04-13 14:09:33 -0400

    customized theming for Cards

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -2,20 +2,22 @@
 import React from "react";
 // import Material-UI components
 import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@material-ui/core";
+// import component specific Material-UI styling
+import useStyles from "./ProjectCardStyle";
 
 function ProjectCard(props) {
-
+    const classes = useStyles();
     // Destructure props for use in card
     const { imgSrc, title, description, repoLink, pageLink } = props;
 
     return (
-        <Card>
-            <CardHeader title={title} />
-            <CardMedia style={{height: "100px" }} image={imgSrc} />
-            <CardContent>
+        <Card >
+            <CardHeader title={title} className={classes.primaryColor} />
+            <CardMedia style={{height: "150px" }} image={imgSrc} />
+            <CardContent className={classes.lightColor}>
                 <Typography>{description}</Typography>
             </CardContent>
-            <CardActions>
+            <CardActions className={classes.primaryColor}>
                 <Button component="a" href={repoLink}>Repository</Button>
                 <Button component="a" href={pageLink}>Page</Button>
             </CardActions>

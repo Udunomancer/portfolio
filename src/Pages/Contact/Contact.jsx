21d0c1f1f7aee85973eed4af098f254a98621ceb
Kyle Chriscoe <kyle717@gmail.com>
2021-04-15 17:17:45 -0400

    added State to message values

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,19 +1,57 @@
 // import default React components
-import React from "react";
+import React, { useState } from "react";
 // import Material-UI components
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+// import emailJS package
+import * as emailjs from "emailjs-com";
 import resume from "../../Assets/Resume.pdf";
 import "./Contact.css";
 
 function Contact() {
-  // const [message, setMessage] = useState({
-  //     subject: "",
-  //     body: "",
-  //     href: ""
-  // })
+  const [message, setMessage] = useState({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let templateParams = {
+      from_name: message.email,
+      to_name: "kyle717+portfolio@gmail.com",
+      subject: message.subject,
+      message_html: message.message,
+    }
+
+    emailjs.send(
+      "gmail",
+      "template_7aq6hjl",
+      templateParams,
+      "user_8aUtJJm2xTXAI6QnyUFCr"
+    );
+  }
+
+  function handleChange(param, value) {
+    setMessage({
+      ...message,
+      [param]: value
+    })
+  }
+
+  function resetForm() {
+    setMessage({
+      name: "",
+      email: "",
+      subject: "",
+      message: ""
+    });
+  }
 
   // function handleInputChange(event) {
   //     const name = event.target.name;
@@ -26,7 +64,30 @@ function Contact() {
     <Paper>
       <Grid container>
         <Grid item xs={12}>
-          <Typography>Visit </Typography>
+          <Typography>Message me directly</Typography>
+        </Grid>
+        <form noValidate autoComplete="off" style={{ width: "100%" }}>
+          <Grid container item xs={12}>
+            <Grid container item xs={4}>
+              <Grid item xs={12}>
+                <TextField id="name" label="Name" value={message.name} onChange={(e) => {handleChange(e.target.id, e.target.value)}} required fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField id="email" label="Email" value={message.email} onChange={(e) => {handleChange(e.target.id, e.target.value)}} required fullWidth />
+              </Grid>
+            </Grid>
+            <Grid container item xs={8}>
+              <Grid item xs={12}>
+                <TextField id="subject" label="Subject" value={message.subject} onChange={(e) => {handleChange(e.target.id, e.target.value)}} required fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField id="message" label="Message" value={message.message} onChange={(e) => {handleChange(e.target.id, e.target.value)}} required fullWidth />
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+        <Grid item xs={12}>
+          <Typography>View my GitHub/LinkedIn</Typography>
           <Divider />
         </Grid>
         <Grid container item xs={12}>
@@ -42,12 +103,12 @@ function Contact() {
             </Grid>
           </Grid>
           <Grid container item xs={12} sm={6}>
-              <Grid item xs={12}>
-                  <Typography>Email:</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                  <Typography>Phone:</Typography>
-              </Grid>
+            <Grid item xs={12}>
+              <Typography>Email:</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Phone:</Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>

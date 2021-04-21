a8e4927e425440a6687cfb344f953f91389187e6
Kyle Chriscoe <kyle717@gmail.com>
2021-04-21 16:41:17 -0400

    displaying resume on My Resume

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,17 +1,18 @@
 // import default React components
 import React from "react";
 // import React-pdf components for resume display
-import { Document, Page } from "react-pdf";
+import { Document } from "react-pdf/dist/esm/entry.webpack";
+import { Page } from "react-pdf";
 // import Resume pdf to display
 import resume from "../../Assets/Resume.pdf";
 
 function Resume() {
 
-    return (
-        <Document file={resume} onLoadError={console.error}>
-            <Page pageNumber={1} />
-        </Document>
-    );
+  return (
+    <Document file={resume}>
+      <Page pageNumber={1} />
+    </Document>
+  );
 }
 
 export default Resume;
